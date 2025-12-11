02f0513176a38eeebd0ad86dfae82b4dc2ff30d7
lim-yubin <yubineric@gmail.com>
2025-12-12 02:14:36 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+function App() {
+  return (
+    <>
+      <div className="bg-red-500">Hello World</div>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
